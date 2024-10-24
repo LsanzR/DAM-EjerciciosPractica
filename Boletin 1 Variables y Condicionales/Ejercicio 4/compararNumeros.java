2327a0c98849b34e09fa911423e8ca1d1fe9a44c
Luis Sanz <luis.sanzr26@icloud.com>
2024-10-24 13:35:39 +0200

    Comparacion de dos numeros. v1.1 Comentarios implementados en el programa.

--- Boletin 1 Variables y Condicionales/Ejercicio 4/compararNumeros.java
+++ Boletin 1 Variables y Condicionales/Ejercicio 4/compararNumeros.java
@@ -1,7 +1,6 @@
 
 
 
-
 // Pedir dos números y decir si son iguales o no.
 
 
@@ -9,27 +8,29 @@ import java.util.Scanner;
 public class compararNumeros {
 public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    double numero1 = primerNumero(input);
-    double numero2 = segundoNumero(input);
-    imprimirComparacion(numero1, numero2);
-    input.close();
+    double numero1 = primerNumero(input); // Solicitamos al usuario que introduzca el primer numero de la comparacion a traves del metodo.
+    double numero2 = segundoNumero(input); // Solicitamos al usuario que introduzca el segundo numero de la comparacion a traves del metodo.
+    imprimirComparacion(numero1, numero2); // Metodo que compara los dos numeros introducidos por el usuario.
+    input.close(); // Cerramos la lectura del terminal.
     }
-
+    // Metodo con el que solicitamos al usuario que introduzca el primer numero
     public static double primerNumero(Scanner input) {
         System.out.println("Porfavor introduce el primer numero para realizar la comparacion : ");
         double numero1 = input.nextDouble();
         return numero1;
     }
-
+    // Metodo con el que solicitamos al usuario que introduzca el segundo numero.
     public static double segundoNumero(Scanner input) {
         System.out.println("Porfavor introduce el segundo numero para realizar la comparacion : ");
         double numero2 = input.nextDouble();
         return numero2;
     }
+    // Metodo con el que realizamos la comparacion de ambos numeros.
     public static void imprimirComparacion(double numero1, double numero2) {
-        if (numero1 == numero2) {
+        // la comparacion la realizamos a traves de un if-else.
+        if (numero1 == numero2) { // Si son iguales :
             System.out.println("Los numeros son iguales");
-        } else {
+        } else { // Si no :
             System.out.println("Los numeros son diferentes");
         }
 
