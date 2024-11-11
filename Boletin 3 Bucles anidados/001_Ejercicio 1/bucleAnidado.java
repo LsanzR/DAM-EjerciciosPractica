515306ae7987928bc05c0a800e252aa29d5f7cd0
Luis Sanz <luis.sanzr26@icloud.com>
2024-11-11 20:30:24 +0100

    .

---
+++ Boletin 3 Bucles anidados/001_Ejercicio 1/bucleAnidado.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class bucleAnidado {
+    public static List<Integer> calcularSuma() {
+        List<Integer> resultados = new List<Integer>() {
+            return resultados;
+        };
+        return resultados;
+    }
+    
+}
+    
+
