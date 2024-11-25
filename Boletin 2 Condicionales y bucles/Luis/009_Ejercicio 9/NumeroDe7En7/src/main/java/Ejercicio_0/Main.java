184aef0cc2b889a40e21a5d20124ee977286cc37
Luis Sanz <luis.sanzr26@icloud.com>
2024-11-25 13:48:57 +0100

    Ejercicio 009 hecho con un for. Escribir de 7 en 7 desde 100 hasta 0.

---
+++ Boletin 2 Condicionales y bucles/Luis/009_Ejercicio 9/NumeroDe7En7/src/main/java/Ejercicio_0/Main.java
@@ -0,0 +1,10 @@
+package Ejercicio_0;
+
+public class Main {
+    public static void main(String[] args) {
+        int numero = 100;
+        for (int i = 100; i >= 0 ; i -= 7) {
+            System.out.println(i);
+        }
+    }
+}
